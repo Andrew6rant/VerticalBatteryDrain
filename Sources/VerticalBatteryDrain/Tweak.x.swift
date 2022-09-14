81dc130194dc93a1fdb270d3a88080e5303ef17b
Andrew Grant <andrew.grant@mail.rit.edu>
2022-09-14 00:13:37 -0400

    properly do battery fill math
    lol

--- Sources/VerticalBatteryDrain/Tweak.x.swift
+++ Sources/VerticalBatteryDrain/Tweak.x.swift
@@ -5,8 +5,8 @@ import VerticalBatteryDrainC
 class BatteryFillHook: ClassHook<_UIBatteryView> {
     func setFillLayer(_ layer: CALayer) {
         orig.setFillLayer(layer)
-        let adjustedLevel = CGFloat(UIDevice.current.batteryLevel) * (22/3)
-        target.fillLayer.setFrame(CGRect(x: 2, y: adjustedLevel + 2, width: 18, height: adjustedLevel))
+        let adjustedLevel = CGFloat(UIDevice.current.batteryLevel) * (22/3.0)
+        target.fillLayer.setFrame(CGRect(x: 2, y: -adjustedLevel + 9, width: 18, height: adjustedLevel))
         target.fillLayer.cornerRadius = 1
     }
     func _updateFillLayer() {}
