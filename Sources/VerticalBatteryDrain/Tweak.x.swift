6c008b19ea9d20ad3b1be40a3f231f403f5a851e
Andrew Grant <andrew.grant@mail.rit.edu>
2022-09-14 01:30:08 -0400

    properly update battery view
    previously, this only updated on a respring.

--- Sources/VerticalBatteryDrain/Tweak.x.swift
+++ Sources/VerticalBatteryDrain/Tweak.x.swift
@@ -5,10 +5,15 @@ import VerticalBatteryDrainC
 class BatteryFillHook: ClassHook<_UIBatteryView> {
     func setFillLayer(_ layer: CALayer) {
         orig.setFillLayer(layer)
-        let adjustedLevel = CGFloat(UIDevice.current.batteryLevel) * (22/3.0)
-        target.fillLayer.setFrame(CGRect(x: 2, y: -adjustedLevel + 9, width: 18, height: adjustedLevel))
+        fillLogic()
         target.fillLayer.cornerRadius = 1
     }
-    func _updateFillLayer() {}
-    func __updateFillLayer() {}
+    func _updateFillLayer() {
+        fillLogic()
+    }
+    // orion:new
+    func fillLogic() {
+        let adjustedLevel = CGFloat(UIDevice.current.batteryLevel) * (22/3.0)
+        target.fillLayer.setFrame(CGRect(x: 2, y: -adjustedLevel + 9, width: 18, height: adjustedLevel))
+    }
 }
